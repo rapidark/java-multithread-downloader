b5ef6fdf1a1148f44d6c338170a0ddb7afe8f735
Darkness <darkness.itachi@gmail.com>
2016-11-01 17:24:53 +0800

    dd

--- src/main/java/com/zhan_dui/download/Test.java
+++ src/main/java/com/zhan_dui/download/Test.java
@@ -17,16 +17,15 @@ public class Test {
 		/*** type you save direcotry ****/
 		String saveDirectory = "";
 		try {
-			DownloadMission mission = new DownloadMission(qQString,
-					saveDirectory, "test1");
+			DownloadMission mission = new DownloadMission(qQString, saveDirectory, "test1");
 			downloadManager.addMission(mission);
-			DownloadMission mission2 = new DownloadMission(google,
-					saveDirectory, "test2");
+			DownloadMission mission2 = new DownloadMission(google, saveDirectory, "test2");
 			downloadManager.addMission(mission2);
-			DownloadMission mission3 = new DownloadMission(big, saveDirectory,
-					"test3");
+			DownloadMission mission3 = new DownloadMission(big, saveDirectory, "test3");
 			downloadManager.addMission(mission3);
+			
 			downloadManager.start();
+			
 			int counter = 0;
 			while (true) {
 				// System.out.println("The mission has finished :"
